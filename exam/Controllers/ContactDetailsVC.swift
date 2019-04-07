eb24bd45a42258dc64a3a14423fdabc3bf0bc91f
Liu Fan Wei <>
2019-04-07 09:56:32 +0800

    change project struct

---
+++ exam/Controllers/ContactDetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  ContactDetailsVC.swift
+//  exam
+//
+//  Created by Liu Fan Wei on 7/4/19.
+//  Copyright © 2019 Liu Fan Wei. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ exam/Extension/UIView.swift
@@ -0,0 +1,9 @@
+//
+//  UIView.swift
+//  exam
+//
+//  Created by Liu Fan Wei on 7/4/19.
+//  Copyright © 2019 Liu Fan Wei. All rights reserved.
+//
+
+import Foundation
