104bfcce5bbf75365bf1ecc0ba7165d551fd0ede
Liu Fan Wei <>
2019-04-07 09:48:37 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'exam' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for exam
+  pod 'IQKeyboardManagerSwift', '6.2.1'
+end
---
+++ exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exam
+//
+//  Created by Liu Fan Wei on 7/4/19.
+//  Copyright © 2019 Liu Fan Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
